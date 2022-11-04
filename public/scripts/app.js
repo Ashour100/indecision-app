38bd3b6ed43235382b2e1c35471d4415013286d1
Ashour100 <interinstitute5@gmail.com>
2022-11-04 17:03:54 +0100

    Events

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,51 @@
 'use strict';
 
-console.log('First Try');
+var number = 0;
 
-var app = {
-    title: 'React',
-    subtitle: 'Subtitle',
-    options: [1, 2]
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count:',
+            number
+        ),
+        React.createElement(
+            'button',
+            { onClick: function onClick() {
+                    number++;render();
+                } },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: function onClick() {
+                    number--;render();
+                } },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: function onClick() {
+                    number = 0;render();
+                } },
+            'Reset'
+        )
+    );
+    ReactDOM.render(template, appRoot);
 };
-
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'h3',
-        null,
-        app.subtitle
-    ),
-    app.options && app.options.length > 0 ? React.createElement(
-        'p',
-        null,
-        'There are ',
-        app.options.length,
-        ' options'
-    ) : React.createElement(
-        'p',
-        null,
-        'No options are available'
-    )
-);
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+render();
 
 //Testing
-var multipier = {
-    numbers: [5, 10, 15],
-    multiplyBy: 2,
-    multipingProcess: function multipingProcess() {
-        var _this = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this.multiplyBy;
-        });
-    }
-};
-console.log(multipier.multipingProcess());
+// const multipier={
+//     numbers:[5,10,15],
+//     multiplyBy:2,
+//     multipingProcess(){
+//         return this.numbers.map((number)=> number*this.multiplyBy);
+//     } 
+// }
+//     console.log(multipier.multipingProcess());
--- src/app.js
+++ src/app.js
@@ -1,31 +1,33 @@
-console.log('First Try');
-
-let app={
-    title:'React',
-    subtitle:'Subtitle',
-    options:[1,2],
-};
 
 
 
-let template=
-<div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <h3>{app.subtitle}</h3>}
-    {(app.options && app.options.length>0) ? <p>There are {app.options.length} options</p> : <p>No options are available</p>}
-</div>;
+let number=0;
+
+
+const render= function(){
+    let template=
+    <div>
+    <h1>Count:{number}</h1>
+    <button onClick={()=>{number++;render()}}>+1</button>
+    <button onClick={()=>{number--;render()}}>-1</button>
+    <button onClick={()=>{number=0;render()}}>Reset</button>
+    </div>;
+    ReactDOM.render(template,appRoot);
+}
 
 const appRoot=document.getElementById('app');
-ReactDOM.render(template,appRoot);
+render();
+
+
 
 
 
 //Testing
-const multipier={
-    numbers:[5,10,15],
-    multiplyBy:2,
-    multipingProcess(){
-        return this.numbers.map((number)=> number*this.multiplyBy);
-    } 
-}
-    console.log(multipier.multipingProcess());
+// const multipier={
+//     numbers:[5,10,15],
+//     multiplyBy:2,
+//     multipingProcess(){
+//         return this.numbers.map((number)=> number*this.multiplyBy);
+//     } 
+// }
+//     console.log(multipier.multipingProcess());
