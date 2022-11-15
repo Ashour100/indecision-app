386c2724325f4e9534c799d806822d089af796bb
Ashour100 <interinstitute5@gmail.com>
2022-11-15 14:20:14 +0100

    app.js divided into components

---
+++ src/buildIt/person.js
@@ -0,0 +1,7 @@
+
+
+import isSenior, { canDrink, isAdult } from "./utils";
+
+console.log(isAdult(18))
+console.log(canDrink(18))
+console.log(isSenior(65))
--- src/person.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import isSenior, { canDrink, isAdult } from "./utils";
-
-console.log(isAdult(18))
-console.log(canDrink(18))
-console.log(isSenior(65))
