349974031a54047f4f3c8aa21db1c7876a7101cc
Ashour100 <interinstitute5@gmail.com>
2022-11-15 12:21:29 +0100

    Imports and exports

--- src/person.js
+++ src/person.js
@@ -1,1 +1,8 @@
-console.log('test')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import isSenior, { canDrink, isAdult } from "./utils";
+
+console.log(isAdult(18))
+console.log(canDrink(18))
+console.log(isSenior(65))
